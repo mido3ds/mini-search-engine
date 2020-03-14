c0c6c3cc0f49df809bcd5365a0e96d0261a9548c
Mahmoud Adas <mido3ds@gmail.com>
2020-03-14 13:03:06 +0200

    reformatting

---
+++ server/src/main/java/com/cufe/searchengine/Server.java
@@ -0,0 +1,49 @@
+package com.cufe.searchengine;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.cufe.searchengine", "com.cufe.searchengine.api", "com.cufe.searchengine.configuration"})
+public class Server implements CommandLineRunner {
+
+	public static void main(String[] args) throws Exception {
+		new SpringApplication(Server.class).run(args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		if (arg0.length > 0 && arg0[0].equals("exitcode")) {
+			throw new ExitException();
+		}
+	}
+
+	@Bean
+	public WebMvcConfigurer webConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("*")
+					.allowedHeaders("Content-Type");
+			}
+		};
+	}
+
+	static class ExitException extends RuntimeException implements ExitCodeGenerator {
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public int getExitCode() {
+			return 10;
+		}
+
+	}
+}
