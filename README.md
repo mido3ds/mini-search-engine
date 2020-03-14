7856564b51f20d938299c4139930a534d46f9ec6
Mahmoud Adas <mido3ds@gmail.com>
2020-03-14 12:24:23 +0200

    added openapi.yaml and generated server and web with it

--- README.md
+++ README.md
@@ -2,10 +2,28 @@
 Mini Search Engine - Advanced Programming Techniques Course Project - Cairo Uni.
 
 # Install Dependencies
+
+* maven +3.6.0
+* java +11.0.3
+* javac +11.0.3
+
+# Run Server
+
+in server dir: `$ mvn spring-boot:run -Dserver.port=<PORT>` where `<PORT>` is the port to bind to.
+
+# Run Crawler
+
 TODO
 
-# Run
+# Run Server Tests
+
+in server dir: `$ mvn test` 
+
+# Run Crawler Tests
+
+in crawler dir: `$ mvn test` 
+
+# Run Web Tests
+
 TODO
 
-# Run Tests
-TODO
---
+++ server/src/main/java/org/openapitools/api/QueryApiController.java
@@ -0,0 +1,25 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-03-14T12:05:55.435057+02:00[Africa/Cairo]")
+
+@Controller
+@RequestMapping("${openapi.miniSearchEngine.base-path:}")
+public class QueryApiController implements QueryApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public QueryApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
