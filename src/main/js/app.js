f310d530a527ace5edebd56ff87e21e5dc3e81ae
Mahmoud Adas <mido3ds@gmail.com>
2020-03-23 02:01:30 +0200

    pages with routing

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
+import {
+    BrowserRouter as Router,
+    Switch, Route, Redirect, useLocation
+} from "react-router-dom"
+import qs from 'qs'
 
 import { DefaultApi } from './api'
 
 const api = new DefaultApi()
 
-const Index = () => {
-    const [content, setContent] = useState([])
+const SearchResult = ({ r }) => {
+    return (
+        <div>
+            <a href={r.link}>
+                <div>{r.title}</div>
+                {r.link}<br />
+            </a>
+            <div>{r.snippet}</div><br />
+        </div>
+    )
+}
+
+const QueryPage = () => {
+    const [results, setResults] = useState([])
+
+    const { q, p } = qs.parse(useLocation().search, { ignoreQueryPrefix: true })
 
     useEffect(() => {
-        api.query("1").then(resp => {
+        api.query(q, p).then(resp => {
             if (resp.status === 200) {
-                console.log(resp.data);
-                setContent(resp.data)
+                setResults(resp.data)
             } else {
                 console.error(`error in query, resp.status={resp.status}`)
             }
@@ -21,19 +39,41 @@ const Index = () => {
 
     return (
         <div>
-            {
-            content.map((c, i) =>
-                <div key={i}>
-                    <a href={c.link}>
-                        <div>{c.title}</div>
-                        {c.link}<br/>
-                    </a>
-                    <div>{c.snippet}</div><br/>
-                </div>
-            )
-            }
+            {results.map((r, i) => <SearchResult r={r} key={i} />)}
         </div>
     )
 }
 
-render(<Index />, document.getElementById('app-root'))
+const HomePage = () => {
+    return (
+        // TODO: search bar
+        <Redirect to="/query?q=ad" />
+    )
+}
+
+const App = () => {
+    return (
+        <Router>
+            {/* TODO: HEADER */}
+            <div>HEADER</div>
+
+            <Switch>
+                <Route path="/query">
+                    <QueryPage />
+                </Route>
+
+                <Route path="/">
+                    <HomePage />
+                </Route>
+            </Switch>
+
+            {/* TODO FOOTER */}
+            <div>FOOTER</div>
+        </Router>
+    )
+}
+
+render(
+    <App />,
+    document.getElementById('app-root')
+)
