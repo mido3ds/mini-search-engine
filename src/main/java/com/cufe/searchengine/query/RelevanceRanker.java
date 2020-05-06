4f73356377efdc46b43ac7df1babb78b3981374f
darkgeek <mohamedshawky911@gmail.com>
2020-05-07 00:48:05 +0200

    relevance ranker log mistake

--- src/main/java/com/cufe/searchengine/query/RelevanceRanker.java
+++ src/main/java/com/cufe/searchengine/query/RelevanceRanker.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RelevanceRanker {
-	private static final Logger log = LoggerFactory.getLogger(PopularityRanker.class);
+	private static final Logger log = LoggerFactory.getLogger(RelevanceRanker.class);
 
 	public static void rank() {
         // TODO: implement relevance rank (TF-IDF)
