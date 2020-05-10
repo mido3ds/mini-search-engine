89e7ecb75680f24af6647d8de7a11d0a4203afc6
darkgeek <mohamedshawky911@gmail.com>
2020-05-11 00:07:21 +0200

    added keyword count to db

--- src/main/java/com/cufe/searchengine/db/table/KeywordsTable.java
+++ src/main/java/com/cufe/searchengine/db/table/KeywordsTable.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.*;
 
 @Component
 public class KeywordsTable {
@@ -13,7 +13,8 @@ public class KeywordsTable {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
-	public void insertOrIgnore(List<String> words, long docID) throws Exception {
+	public void insertOrIgnore(Map<String, Integer> wordFreq, long docID) throws Exception {
+		List<String> words = new ArrayList<String>(wordFreq.keySet());
 		final StringBuilder builder = new StringBuilder("INSERT OR IGNORE INTO keywords VALUES");
 		for (int i = 0; i < words.size(); i++) {
 			builder.append("(?)");
@@ -44,9 +45,9 @@ public class KeywordsTable {
 //		log.info("rowids.size() = " + rowids.size());
 
 		final StringBuilder builder3 = new StringBuilder();
-		builder3.append("REPLACE INTO keywords_documents(docID, wordID) VALUES");
+		builder3.append("REPLACE INTO keywords_documents(docID, wordID, count) VALUES");
 		for (int i = 0; i < rowids.size(); i++) {
-			builder3.append("(").append(docID).append(",").append(rowids.get(i)).append(")");
+			builder3.append("(").append(docID).append(",").append(rowids.get(i)).append(",").append(wordFreq.get(words.get(i))).append(")");
 
 			if (i != rowids.size() - 1) {
 				builder3.append(",");
