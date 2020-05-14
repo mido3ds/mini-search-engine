30ba7a4c442e29209b4d0891388fafe46a1d66e7
Evraa <evramyousef@gmail.com>
2020-05-14 07:43:56 +0200

    added base code for Trends page

--- src/main/js/pages/trends/index.js
+++ src/main/js/pages/trends/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
+import Pagination from '@material-ui/lab/Pagination'
+import qs from 'qs'
+import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
+import { useLocation } from "../../use-location"
+import { DefaultApi } from '../../api'
+import CommonSearchBar from '../../common-search-bar'
+import SearchResult from '../../search-result'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
 
     // ["Mahmoud Adas",
     // "Mahmoud Othman Adas",
@@ -11,13 +19,94 @@ import { render } from 'react-dom'
     // "No not adas again",
     // "Adas (✿⁠´ ꒳ ` )"]
 
-const Index = () => (
-    <div>
-        Test, remove this line
-    </div>
-)
-
-render(
-    <Index />,
-    document.getElementById('app-root')
-)
+    
+    // This page shows:
+    //     common search bar (sticks on the top)
+    //     results of regular queries
+    
+    //To be developed:
+        //styling the background color of the nav
+    
+    const API = new DefaultApi()
+    
+    const Results = () => {
+        const [results, setResults] = useState([])
+        
+        const [country, setQ] = useState("")
+        const [err, setErr] = useState("")
+    
+        const { search } = useLocation()
+        
+        //I don't know the usage of this code!
+        useEffect(() => {
+            const parsed = qs.parse(search, { ignoreQueryPrefix: true })
+            setQ(parsed.country)
+        }, [search])
+    
+        useEffect(() => {
+            if (country !== "") {
+                API.trends(country)
+                    .then(resp => {
+                        if (resp.status === 200) {
+                            setResults(resp.data.results)
+                            setErr("")
+                        } else {
+                            setErr("No Trending Results")
+                        }
+                    })
+                    .catch(error => {
+                        setErr("No Trending Results")
+                    })
+            }
+        }, [country])
+        
+        if (err) {
+            return (
+                <div style = {errStyle} >
+                     <h2>{err}</h2>
+                </div>
+            )
+        } else {
+            return (
+                <div >
+                    <h1>ev</h1>
+                    {/* {results.map((r, i) => <SearchResult r={r} image = {false} key={i} />)}
+                    <Pagination
+                        count={allPages}
+                        color="primary"  
+                        shape="rounded" 
+                        page={currPage} 
+                        onChange={(_, page) => setP(page)} 
+                        style = {pageStyle}
+                    />
+                     */}
+                </div>
+            )
+        }
+    }
+    
+    const pageStyle = {
+        marginTop: "30px",
+        marginBottom: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+    
+    const errStyle = {
+        marginTop: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+    
+    const Index = () => (
+        
+            <Results />
+    )
+    
+    render(
+        <Index />,
+        document.getElementById('app-root')
+    )
+    
